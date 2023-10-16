7543a5a3227837831b3a19c4739ad5001fd575d1
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-10-16 09:59:59 +0530

    for-factorial-task-2

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+document.write("To check the factorial up(1!    to     10!"+"<br>");
+document.write("---------------------------------"+"<br>")
+document.write("The factorial of (1!-10!) is:"+"<br>");
+
+let fact=1;
+for(let i=1;i<=10;i++)
+{
+    fact=fact*i;
+    
+    document.write(fact+"<br>");
+}
+
